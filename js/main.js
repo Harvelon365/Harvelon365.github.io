e9eb2cfd1e0a9221fed0938be95c7cc5ae002dfd
Harvelon365 <Harvelon@outlook.com>
2023-06-01 22:44:28 +0100

    Fixed some issues with mobile animation compatability

--- js/main.js
+++ js/main.js
@@ -5,13 +5,16 @@ const textload = () => {
         text.textContent = "Harvey Tucker."
     }, 0);
     setTimeout(() => {
-        text.textContent = "a student."
+        if (window.innerWidth < 1050) {text.textContent = "Harvey Tucker"}
+        else {text.textContent = "a student."}
     }, 4000);
     setTimeout(() => {
-        text.textContent = "a programmer."
+        if (window.innerWidth < 1050) {text.textContent = "Harvey Tucker"}
+        else {text.textContent = "a programmer."}
     }, 8000);
     setTimeout(() => {
-        text.textContent = "a legend."
+        if (window.innerWidth < 1050) {text.textContent = "Harvey Tucker"}
+        else {text.textContent = "a developer."}
     }, 12000);
 }
 
