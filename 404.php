f0e336013f46095d330e2d42ec0fccf6c9321e7f
Harvey Tucker <psyht7@exmail.nottingham.ac.uk>
2025-02-18 08:08:20 +0000

    convert all pages to php

---
+++ 404.php
@@ -0,0 +1,5 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/nav.html"); ?>
+<?php echo file_get_contents("html/404.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+
---
+++ post.php
@@ -0,0 +1,5 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/nav.html"); ?>
+<?php echo file_get_contents("html/post.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+
